c35a92d447a31a86b9490a9e8b5b1d00b91989dd
bananaboat <937499953@qq.com>
2023-06-01 21:30:15 +0800

    feat: 首页样式完成

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ConfigProvider } from "antd";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: "#EB4A72",
+      },
+    }}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ConfigProvider>
 );
--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,7 +1,66 @@
+import styles from "./index.module.scss";
+import logo from "../../assets/terminal_white.svg";
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
+import { Button, Input } from "antd";
+import { useNavigate } from "react-router-dom";
 interface IProps {}
 
 function HomePage({}: IProps) {
-  return <>Home</>;
+  const navigate = useNavigate();
+  const descDivRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const typed = new Typed(descDivRef.current, {
+      strings: [
+        "告别终端环境配置，浏览器环境开箱即用^600 <br />AI机器人为你解答一切关于Linux命令的疑问",
+      ],
+      typeSpeed: 50,
+    });
+
+    return () => typed.destroy();
+  });
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.header}>
+          <div className={styles.title}>
+            <span>Terryminal</span>
+          </div>
+          <p className={styles.subtitle}>在线终端命令学习平台</p>
+          <div>
+            <span ref={descDivRef} className={styles.desc} />
+          </div>
+        </div>
+
+        <div className={styles.login}>
+          <Input className={styles.input} placeholder="请输入邮箱" />
+          <Input className={styles.input} placeholder="请输入密码" />
+
+          <div className={styles.btnGroup}>
+            <Button
+              onClick={() => navigate("/learn")}
+              type="primary"
+              size="large"
+              shape="round"
+            >
+              登录
+            </Button>
+            <Button size="large" shape="round">
+              注册
+            </Button>
+            <Button type="link">忘记密码？</Button>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.right}>
+        <div className={styles.logoBg} />
+        <img src={logo} className={styles.logo} alt="logo" />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
